3aa898ecaaca4b2b53d30470a993376806bbce4f
Deeya2411 <31283380+Deeya2411@users.noreply.github.com>
2024-06-30 15:37:07 +0400

    Update Exercise6.py

--- Week2/Day2/Exercice XP/Exercice XP/Exercise6.py
+++ Week2/Day2/Exercice XP/Exercice XP/Exercise6.py
@@ -1,16 +1,21 @@
-magician_names = ['Harry Houdini', 'David Blaine', 'Criss Angel'] 
-
 def show_magicians(magicians):
     for magician in magicians:
-        print(magician.title())
-
-show_magicians(magician_names)
+        print(magician)
 
 def make_great(magicians):
-	for magician in magicians:
-		return ("The Great " +  magician.title())
+    great_magicians = []
 
-print (make_great(magician_names))
+    while magicians:
+        magician = magicians.pop()
+        great_magician = 'The Great ' + magician 
+        great_magicians.append(great_magician)
 
+    for great_magician in great_magicians:
+        magicians.append(great_magician)
 
+magician_names = ['Harry Houdini', 'David Blaine', 'Criss Angel']
+show_magicians(magician_names)
 
+print("\n")
+make_great(magician_names)
+show_magicians(magician_names)

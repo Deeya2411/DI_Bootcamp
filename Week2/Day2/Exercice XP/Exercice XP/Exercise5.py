f61ff9a2c48d3a3b064cbc095e51f705707ee055
Deeya2411 <31283380+Deeya2411@users.noreply.github.com>
2024-06-30 15:45:48 +0400

    Update Exercise5.py

--- Week2/Day2/Exercice XP/Exercice XP/Exercise5.py
+++ Week2/Day2/Exercice XP/Exercice XP/Exercise5.py
@@ -4,22 +4,11 @@ text = input("Enter the text of message to be printed: ")
 
 
 def make_shirt(size, text):
-	
-	size = "Large"
-	
-	text = "I love Python"
-
-	if (size == "Large" or size == "L"):
-		
-		text = "I love Python"
-
-	elif (size == "Medium" or size == "M"):
-		
-		text = "I love Python" 	
 
 	print(f"The size of the shirt is {size} and the text is {text}")
 
-output = make_shirt(size, text)
+make_shirt("Large", "I love Python")
 
+make_shirt("Medium", "I love Python")
 
-
+make_shirt(size, text)
